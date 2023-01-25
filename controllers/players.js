a66621cf668ad207aeea3cff32588d833df65f8e
Logan <logkauf14@gmail.com>
2023-01-25 14:37:10 -0500

    adds completed controller files

--- controllers/players.js
+++ controllers/players.js
@@ -4,28 +4,72 @@ const Player = require('../models/player')
 
 //index
 
-
+router.get('/players', function(req, res) {
+    Player.find({}, function(error, allPlayers) {
+        res.render('players/index.ejs', {
+            title: 'Players',
+            players: allPlayers
+        })
+    })
+})
 
 //new
 
-
+router.get('/players/new', function(req, res) {
+    res.render('players/new.ejs', {
+        title: 'Add New Player'
+    })
+})
 
 //delete
 
-
+router.delete('/players/:id', function(req, res) {
+    Player.findByIdAndDelete(req.params.id, function(error, deletedPlayer) {
+        res.redirect('/players')
+    })
+})
 
 //update
 
-
+router.put('/products/:id', function(req, res) {
+    Player.findByIdAndUpdate(req.params, req,body, 
+        {
+            new: true,
+        },
+        function(error, updatedPlayer) {
+            res.redirect(`${req.params.id}`)
+        }
+        )
+})
 
 //create
 
-
+router.post('/players', function(req, res) {
+    Player.create(req.body, function(error, createdPlayer) {
+        res.redirect('/players')
+    })
+})
 
 //edit
 
-
+router.get('/players/:id/edit', function(req, res) {
+    Player.findById(req.params.id, function(error, editPlayer) {
+        res.render('players/edit.ejs', {
+            title: 'Edit Player',
+            player: editPlayer
+        })
+    })
+})
 
 //show
+
+router.get('/players/:id', function(req, res) {
+    Player.findById(req.params.id, function(error, foundPlayer) {
+        res.render('players/show.ejs', {
+            title: 'Player Information',
+            player: foundPlayer
+        })
+    })
+})
 
 module.exports = router
--- controllers/teams.js
+++ controllers/teams.js
@@ -1,33 +1,77 @@
 const express = require('express')
+const { appendFile } = require('fs')
 const router = express.Router()
 const Team = require('../models/team')
 
 //index
 
 router.get('/teams', function(req, res) {
-    res.send('hi')
+    Team.find({}, function(error, allTeams) {
+        res.render('teams/index.ejs', {
+            teams: allTeams,
+            title: 'Teams'
+        })
+    })
+   
 })
 
 //new
 
-
+router.get('/teams/new', function(req, res) {
+    res.render('teams/new.ejs', {
+        title: 'Add New Team'
+    })
+})
 
 //delete
 
-
+router.delete('/teams/:id', function(req, res) {
+    Team.findByIdAndDelete(req.params.id, function(error, deletedTeam) {
+        res.redirect('/teams')
+    })
+})
 
 //update
 
-
+router.put('/teams/:id', function(req, res) {
+    Team.findByIdAndUpdate(req.params.id, req.body, 
+        {
+            new: true,
+        },
+        function(error, updatedTeam) {
+            res.redirect(`${req.params}`)
+        })
+})
 
 //create
 
-
+router.post('/teams', function(req, res) {
+    Team.create(req.body, function(error, createdteam) {
+        res.redirect('/teams')
+    })
+})
 
 //edit
 
-
+router.get('/teams/:id/edit', function(req,res) {
+    Team.findById(req.params.id, function(error, editTeam) {
+        res.render('teams/edit.ejs', {
+            team: editTeam,
+            title: 'Edit Team'
+        })
+    })
+})
 
 //show
+
+router.get('/teams/:id', function(req,res) {
+    Team.findById(req.params.id, function(err, foundTeam) {
+        res.render('teams/show.ejs', {
+            team: foundTeam,
+            index: req.params.id,
+            title: 'Team Information'
+        })
+    })
+})
 
 module.exports = router
